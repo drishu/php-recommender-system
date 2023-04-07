869bd6e634ed11e7be56607e3dfde680f229c119
drishu <aszilagyi@live.com>
2023-04-07 15:16:51 +0300

    Add interface to Encoder.

--- src/Utils/OneHotEncoder.php
+++ src/Utils/OneHotEncoder.php
@@ -4,21 +4,17 @@ declare(strict_types=1);
 
 namespace Drishu\RecommendationSystem\Utils;
 
+use Drishu\RecommendationSystem\Contract\EncoderInterface;
+
 /**
  * Class OneHotEncoder
  *
  * This class is used for performing one-hot encoding on categorical data.
  */
-class OneHotEncoder
+class OneHotEncoder implements EncoderInterface
 {
     /**
-     * Transforms the given string into a one-hot encoded binary vector.
-     *
-     * @param string $data         The input data as a string with values separated by the given separator.
-     * @param array  $uniqueValues The array of unique values that can appear in the data.
-     * @param string $separator    The separator used to split the input data string (default is a comma).
-     *
-     * @return array The one-hot encoded binary vector.
+     * {@inheritDoc}
      */
     public static function transform(string $data, array $uniqueValues, string $separator = ','): array
     {
@@ -29,12 +25,7 @@ class OneHotEncoder
     }
 
     /**
-     * Encodes the given array of values into a one-hot encoded binary vector.
-     *
-     * @param array $values       The input data as an array of values.
-     * @param array $uniqueValues The array of unique values that can appear in the data.
-     *
-     * @return array The one-hot encoded binary vector.
+     * {@inheritDoc}
      */
     public static function encode(array $values, array $uniqueValues): array
     {
