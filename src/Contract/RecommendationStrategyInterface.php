ad2f19d2a16d12323ab566c4537a574b66d96263
drishu <aszilagyi@live.com>
2023-04-07 14:42:51 +0300

    Propper ContentBasedRecommender.

--- src/Contract/RecommendationStrategyInterface.php
+++ src/Contract/RecommendationStrategyInterface.php
@@ -4,7 +4,29 @@ declare(strict_types=1);
 
 namespace Drishu\RecommendationSystem\Contract;
 
+/**
+ * Interface RecommendationStrategyInterface
+ *
+ * This interface represents the contract for recommendation strategies.
+ * It defines methods that any recommendation strategy should implement.
+ */
 interface RecommendationStrategyInterface
 {
-    public function estimateUserRating($movie_id, $user_id, $mysqli);
+    /**
+     * Calculate the similarity score between two items using the features.
+     *
+     * @param array $item1 The first item.
+     * @param array $item2 The second item.
+     * @return float The similarity score between the two items.
+     */
+    public function calculateSimilarity(array $item1, array $item2): float;
+
+    /**
+     * Recommend items for a user based on their past interactions and the similarity between items.
+     *
+     * @param array $userInteractions The user's past interactions with items.
+     * @param array $allItems All available items for recommendation.
+     * @return array The recommended items for the user.
+     */
+    public function recommend(array $userInteractions, array $allItems): array;
 }
--- src/RecommendationStrategy/ContentBasedStrategy.php
+++ src/RecommendationStrategy/ContentBasedStrategy.php
@@ -5,8 +5,71 @@ declare(strict_types=1);
 namespace Drishu\RecommendationSystem\RecommendationStrategy;
 
 use Drishu\RecommendationSystem\Contract\RecommendationStrategyInterface;
+use Drishu\RecommendationSystem\Contract\FeatureInterface;
 
-class ContentBasedStrategy implements RecommendationStrategyInterface
+/**
+ * Class ContentBasedRecommender
+ *
+ * This class implements a content-based recommendation system.
+ * It takes into account features of the items to calculate the similarity between them.
+ * Based on these similarities, the system can recommend items that are similar to the ones
+ * a user has interacted with before.
+ */
+class ContentBasedRecommender implements RecommendationStrategyInterface
 {
+    /**
+     * @var FeatureInterface[] An array of features used to calculate the similarity between items.
+     */
+    protected $features;
 
+    /**
+     * ContentBasedRecommender constructor.
+     *
+     * @param FeatureInterface[] $features An array of features used to calculate the similarity between items.
+     */
+    public function __construct(array $features)
+    {
+        $this->features = $features;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function calculateSimilarity(array $item1, array $item2): float
+    {
+        $totalSimilarity = 0;
+        $totalFeatures = count($this->features);
+
+        foreach ($this->features as $feature) {
+            $transformedItem1 = $feature->transform($item1);
+            $transformedItem2 = $feature->transform($item2);
+            $totalSimilarity += $feature->similarity($transformedItem1, $transformedItem2);
+        }
+
+        return $totalSimilarity / $totalFeatures;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function recommend(array $userInteractions, array $allItems): array
+    {
+        $scores = [];
+
+        foreach ($allItems as $item) {
+            $totalSimilarity = 0;
+
+            foreach ($userInteractions as $interaction) {
+                $totalSimilarity += $this->calculateSimilarity($item, $interaction);
+            }
+
+            $scores[$item['id']] = $totalSimilarity / count($userInteractions);
+        }
+
+        arsort($scores);
+
+        $recommendedItems = array_keys($scores);
+
+        return $recommendedItems;
+    }
 }
